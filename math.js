437e56846687925c10c6e760f7a1f3e0ca91e3f4
jspspike <jspspike@gmail.com>
2019-04-10 18:22:46 -0500

    Added stuff

--- math.js
+++ math.js
@@ -1,7 +1,7 @@
-var houston_total = 25.0;
+var houston_total = 30.0;
 var detroit_total = 5.0;
 
-var houston = {'254': {'name': 'The Cheesy Poofs', 'amount': 10.0, 'img': '254.png', 'per': 1.3636363636363638}, '118': {'name': 'Robonauts', 'amount': 5.0, 'img': '118.png', 'per': 3.333333333333333}, '1619': {'name': 'Up-A-Creek Robotics', 'amount': 5.0, 'img': '1619.jpg', 'per': 3.333333333333333}, '3310': {'name': 'Black Hawk Robotics', 'amount': 5.0, 'img': '3310.png', 'per': 3.333333333333333}, '?': {'name': 'New Team', 'num': '', 'per': 25.0, 'img': 'unknown.png'}};
+var houston = {'254': {'name': 'The Cheesy Poofs', 'amount': 10.0, 'img': '254.png', 'per': 1.8181818181818183}, '118': {'name': 'Robonauts', 'amount': 5.0, 'img': '118.png', 'per': 4.166666666666666}, '1619': {'name': 'Up-A-Creek Robotics', 'amount': 5.0, 'img': '1619.jpg', 'per': 4.166666666666666}, '3310': {'name': 'Black Hawk Robotics', 'amount': 5.0, 'img': '3310.png', 'per': 4.166666666666666}, '148': {'name': 'Robowranglers', 'amount': 5.0, 'img': '148.png', 'per': 4.166666666666666}, '?': {'name': 'New Team', 'num': '', 'per': 30.0, 'img': 'unknown.png'}};
 var detroit = {'2056': {'name': 'OP Robotics', 'amount': 5.0, 'img': '2056.png', 'per': 0.0}, '?': {'name': 'New Team', 'num': '', 'per': 5.0, 'img': 'unknown.png'}};
 
 console.log($("#254").text());
